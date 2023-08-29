388905874b40055498e3aa819223509a24ee08a4
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-08-30 08:18:04 +0900

    how to handle if vec is got iterator and then vec is appended an item.

---
+++ examples/get_vec_iter_then_push_item.rs
@@ -0,0 +1,18 @@
+//! イテレーターを取得した後にベクタを変更するとエラーになる例
+#[allow(unused_mut)]
+
+fn main() {
+    let mut v = vec![1, 2, 3]; // warning: unused_mut
+    let it = v.iter();
+    // error[E0502]: cannot borrow `v` as mutable because it is also borrowed as immutable
+    //  --> examples/get_vec_iter_then_push_item.rs:4:5
+    //   |
+    // 3 |     let it = v.iter();
+    //   |              -------- immutable borrow occurs here
+    // 4 |     v.push(4);
+    //   |     ^^^^^^^^^ mutable borrow occurs here
+    // 5 |     println!("{:?}", it);
+    //   |                      -- immutable borrow later used here
+    // v.push(4);
+    println!("{:?}", it);
+}
