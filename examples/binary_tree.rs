1620da52c8a95ea1700faf6e118da530c993f59b
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-07 15:45:56 +0900

    Binary Tree

---
+++ examples/binary_tree.rs
@@ -0,0 +1,138 @@
+use std::cmp::Ordering;
+
+/// バイナリ・ツリー内のノード
+#[derive(Debug)]
+struct Node<T: Ord> {
+    value: T,
+    left: Subtree<T>,
+    right: Subtree<T>,
+}
+
+/// 空の可能性があるサブツリー
+#[derive(Debug)]
+struct Subtree<T: Ord>(Option<Box<Node<T>>>);
+
+impl<T: Ord> Node<T> {
+    fn new(value: T) -> Self {
+        Self {
+            value,
+            left: Subtree::new(),
+            right: Subtree::new(),
+        }
+    }
+}
+
+impl<T: Ord> Subtree<T> {
+    fn new() -> Self {
+        Self(None)
+    }
+
+    fn insert(&mut self, value: T) {
+        match &mut self.0 {
+            None => self.0 = Some(Box::new(Node::new(value))),
+            Some(n) => match value.cmp(&n.value) {
+                Ordering::Less => n.left.insert(value),
+                Ordering::Equal => {}
+                Ordering::Greater => n.right.insert(value),
+            },
+        }
+    }
+
+    fn has(&self, value: &T) -> bool {
+        match &self.0 {
+            None => false,
+            Some(n) => match value.cmp(&n.value) {
+                Ordering::Less => n.left.has(value),
+                Ordering::Equal => true,
+                Ordering::Greater => n.right.has(value),
+            },
+        }
+    }
+
+    fn len(&self) -> usize {
+        match &self.0 {
+            None => 0,
+            Some(n) => 1 + n.left.len() + n.right.len(),
+        }
+    }
+}
+
+/// バイナリ・ツリーを使用して、値の集合を蓄積するコンテナ
+///
+/// もし同じ値が複数回追加された場合、たった1つしか蓄積されない。
+#[derive(Debug)]
+pub struct BinaryTree<T: Ord> {
+    root: Subtree<T>,
+}
+
+// Implement `new`, `insert`, `len`, and `has`.
+
+impl<T: Ord> BinaryTree<T> {
+    fn new() -> Self {
+        Self {
+            root: Subtree::new(),
+        }
+    }
+
+    fn insert(&mut self, value: T) {
+        self.root.insert(value);
+    }
+
+    fn has(&self, value: &T) -> bool {
+        self.root.has(value)
+    }
+
+    fn len(&self) -> usize {
+        self.root.len()
+    }
+}
+
+fn main() {
+    println!("Hello world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn len() {
+        let mut tree = BinaryTree::new();
+        assert_eq!(tree.len(), 0);
+        tree.insert(2);
+        assert_eq!(tree.len(), 1);
+        tree.insert(1);
+        assert_eq!(tree.len(), 2);
+        tree.insert(2); // not a unique item
+        assert_eq!(tree.len(), 2);
+    }
+
+    #[test]
+    fn has() {
+        let mut tree = BinaryTree::new();
+        fn check_has(tree: &BinaryTree<i32>, exp: &[bool]) {
+            let got: Vec<bool> = (0..exp.len()).map(|i| tree.has(&(i as i32))).collect();
+            assert_eq!(&got, exp);
+        }
+
+        check_has(&tree, &[false, false, false, false, false]);
+        tree.insert(0);
+        check_has(&tree, &[true, false, false, false, false]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(3);
+        check_has(&tree, &[true, false, false, true, true]);
+    }
+
+    #[test]
+    fn unbalanced() {
+        let mut tree = BinaryTree::new();
+        for i in 0..100 {
+            tree.insert(i);
+        }
+        assert_eq!(tree.len(), 100);
+        assert!(tree.has(&50));
+    }
+}
